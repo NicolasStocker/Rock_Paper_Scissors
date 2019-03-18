cd9f0e0e0c601c822a5b0c8335d11877520e1df3
Nicolas Stocker <nicolas.stocker@htl.rennweg.at>
2019-03-18 12:00:07 +0100

    v1.0 First commit - crate Result.php

---
+++ src/Result.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace HTL3R\RPS;
+
+/**
+ * @Entity @Table(name="users")
+ */
+class Result
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $player1;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $player2;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $winner;
+
+    /**
+     * @Column(type="date")
+     * @var date
+     */
+    protected $date;
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getPlayer1()
+    {
+        return $this->player1;
+    }
+
+    public function setPlayer1($name)
+    {
+        $this->player1 = $name;
+    }
+
+    public function getPlayer2()
+    {
+        return $this->player2;
+    }
+
+    public function setPlayer2($name)
+    {
+        $this->player2 = $name;
+    }
+
+    public function getWinner()
+    {
+        return $this->winner;
+    }
+
+    public function setWinner($name)
+    {
+        $this->winner = $name;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+
+}
