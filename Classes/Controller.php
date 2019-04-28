2f843633fcf529cb5515fc1fb78688e64ecf874d
Nicolas Stocker <nicolas.stocker@htl.rennweg.at>
2019-04-28 19:37:55 +0200

    v1.4 Implementing the PHP Controller and the Game

--- Classes/Controller.php
+++ Classes/Controller.php
@@ -6,12 +6,65 @@
  * Time: 12:00
  */
 
-namespace HTL3R\RPS;
 
 
-class Controller
-{
+if(isset($_GET['choice'])){
+
+    $pick = $_GET['choice'];
+}
+else{
+    $pick = 1;
+}
+
+
+header('content-type: application/json');
+echo json_encode(getWinner($pick));
+
+function  getWinner ($pick){
+
+
+    $cpuChoice = rand(1,3);
+
+    $winner = "";
 
 
 
+
+    if($pick == 1){
+        if($cpuChoice == 1){
+            $winner = "tie";
+        }
+        elseif($cpuChoice == 2){
+            $winner = "CPU";
+        }
+        elseif($cpuChoice == 3){
+            $winner = "Player";
+        }
+    }
+    elseif($pick == 2){
+        if($cpuChoice == 1){
+            $winner = "Player";
+        }
+        elseif($cpuChoice == 2){
+            $winner = "tie";
+        }
+        elseif($cpuChoice == 3){
+            $winner = "CPU";
+        }
+    }
+
+    elseif($pick == 3){
+        if($cpuChoice == 1){
+            $winner = "CPU";
+        }
+        elseif($cpuChoice == 2){
+            $winner = "Player";
+        }
+        elseif($cpuChoice == 3){
+            $winner = "tie";
+        }
+    }
+
+
+        return $winner;
 }
